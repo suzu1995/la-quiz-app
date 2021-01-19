e78a24df14a22e3a0ae4f3b99cf026363d070b78
久賀鈴香 <kugasuzuka@kukasuzukanoMacBook-Pro.local>
2021-01-19 12:46:42 +0900

    Laravelへの移管完了

---
+++ laravel_vue_quiz/resources/js/home.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './components/page/Home'
+import VueSocialSharing from 'vue-social-sharing';
+
+Vue.use(VueSocialSharing);
+new Vue({
+  el: 'app',
+  components: {
+    app: App
+  }
+})
--- vue_hello/resources/js/components/layout/TheFooter.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <footer>
-    <div class="container text-center">
-      <small>© 2019 4Answers-quiz. All rights reserved.</small>
-    </div>
-  </footer>
-</template>
