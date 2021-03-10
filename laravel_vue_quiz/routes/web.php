1a750cc88fed2743586236d3575f29825b2c1c05
久賀鈴香 <kugasuzuka@kukasuzukanoMacBook-Pro.local>
2021-03-10 23:13:23 +0900

    会員登録機能実装

--- laravel_vue_quiz/routes/web.php
+++ laravel_vue_quiz/routes/web.php
@@ -21,3 +21,5 @@ Route::get('/quiz', function () {
 Route::get('/register', function () {
   return view('auth.register.index');
 });
+
+Route::post('/register', 'Auth\RegisterController@register');
