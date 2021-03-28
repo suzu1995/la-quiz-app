04eed607bccf707e1c021609b7f8bf294e167468
久賀鈴香 <kugasuzuka@kukasuzukanoMacBook-Pro.local>
2021-03-28 13:41:43 +0900

    depuloy前

--- laravel_vue_quiz/app/Quiz.php
+++ laravel_vue_quiz/app/Quiz.php
@@ -2,21 +2,29 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-use App\Category;
 use App\Answer;
+use App\Category;
+use Illuminate\Database\Eloquent\Model;
 
 class Quiz extends Model
 {
-    protected $table = 'quizzes';
+  protected $table = 'quizzes';
 
-    public function answer()
-    {
-        return $this->hasOne('App\Answer','id', 'answers_id');
-    }
+  public function answer()
+  {
+    return $this->hasOne('App\Answer', 'id', 'answers_id');
+  }
+  public function category()
+  {
+    return $this->hasOne('App\Category', 'id', 'categories_id');
+  }
 
-    public function category()
-    {
-        return $this->hasOne('App\Category', 'id', 'categories_id');
-    }
+  public static function boot()
+  {
+    parent::boot();
+
+    static::deleting(function ($answer_model) {
+      $answer_model->answer()->delete();
+    });
+  }
 }
